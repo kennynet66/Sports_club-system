9c7876b3bb359028b6bc6b00caa26efab6fcd033
Kenny <kennynet66@gmail.com>
2023-08-05 20:44:40 +0300

    Store Items

--- task/core/views.py
+++ task/core/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Sports_details, Patron
+from .models import Sports_details, Patron, Store
 
 def login(request):
     return render(request, 'core/login.html')
@@ -16,4 +16,11 @@ def patrons(request):
     leader = Patron.objects.all()
     return render(request, 'core/patron.html',{
         'leader' : leader
+    })
+
+@login_required
+def storage(request):
+    items = Store.objects.all
+    return render (request, 'core/store.html',{
+        'items' : items
     })
